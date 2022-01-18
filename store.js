3db23c1d42cb01353cd3493d250b3e4f40358540
tailwaggin <rich@bouldertailwaggin.com>
2022-01-18 00:48:32 -0700

    Update store.js

--- store.js
+++ store.js
@@ -38,5 +38,10 @@ function load() {
       break;
   }
 
-  document.getElementById('contents').src = url;
+  const iframe = document.createElement('iframe');
+  iframe.src = url;
+  iframe.style.width = '100%';
+  iframe.style.height = '100vh';
+  iframe.style.border = '0';
+  document.body.appendChild(iframe);
 }
