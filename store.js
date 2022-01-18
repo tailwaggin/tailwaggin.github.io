ad792bee7b54f40948a2838ee2bdf8a9a49adaa2
tailwaggin <rich@bouldertailwaggin.com>
2022-01-18 01:13:58 -0700

    Update store.js

--- store.js
+++ store.js
@@ -40,8 +40,5 @@ function load() {
 
   const iframe = document.createElement('iframe');
   iframe.src = url;
-  iframe.style.width = '100%';
-  iframe.style.height = '100vh';
-  iframe.style.border = '0';
   document.body.appendChild(iframe);
 }
