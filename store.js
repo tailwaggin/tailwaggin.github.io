225f71e1f81623fb29327527f30d8b0118d532a8
tailwaggin <rich@bouldertailwaggin.com>
2022-01-18 00:19:51 -0700

    initial version

---
+++ store.js
@@ -0,0 +1,42 @@
+const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbzH-nbQOTy5n1KMkzCxv5M3inJLucGhRkLNVinPRi_4oy8RV1_ddKQ83OUspG6RjpP_WA/exec';
+const DEFAULT_SALE = 'fruit2022test';
+
+function load() {
+  const params = {
+    sale: DEFAULT_SALE,
+    action: 'start',
+  };
+
+  const queryMatch = window.location.href.match(/\?(.*)/);
+
+  if (queryMatch) {
+    queryMatch[1].split('&').forEach(nameValue => {
+      const nameMatch = nameValue.match(/([^=]+)/);
+      const valueMatch = nameValue.match(/=(.*)/);
+
+      if (nameMatch && valueMatch && valueMatch[1] && valueMatch[1].length < 20) {
+        const name = nameMatch[1];
+        switch (name) {
+          case 'sale':
+          case 'action':
+          case 'order':
+            params[name] = valueMatch[1];
+        }
+      }
+    });
+  }
+
+  const query = Object.entries(params).map(kv => `${kv[0]}=${kv[1]}`).join('&');
+  const url = `${WEBAPP_URL}?${query}`
+
+  switch (params.action) {
+    case 'start':
+      document.title = `${document.title} - Create Order`;
+      break;
+    case 'status':
+      document.title = `${document.title} - Order Status (${params.order})`;
+      break;
+  }
+
+  document.getElementById('contents').src = url;
+}
